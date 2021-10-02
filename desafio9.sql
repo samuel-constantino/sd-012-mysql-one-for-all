624834d7e58b0efdf002584c01a3f6c1e504a65c
Samuel Constantino <samuelicapuidados@gmail.com>
2021-10-01 22:33:39 -0300

    implementa desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,19 @@
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN artista_nome varchar(50))
+BEGIN
+	SELECT
+		ar.artista AS `artista`,
+        al.album AS `album`
+	FROM
+		SpotifyClone.Artista AS ar
+			INNER JOIN
+		SpotifyClone.Album AS al ON ar.artista_id = al.artista_id
+    WHERE
+		`artista` = artista_nome;
+END $$
+
+DELIMITER ;
+
+CALL albuns_do_artista('Walter Phoenix');
