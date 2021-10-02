fcfe3eaae9e0c875ed17316ca4956fd387a34e05
Samuel Constantino <samuelicapuidados@gmail.com>
2021-10-01 22:38:44 -0300

    fix code climate

--- desafio9.sql
+++ desafio9.sql
@@ -3,15 +3,15 @@ USE SpotifyClone;
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN artista_nome varchar(50))
 BEGIN
-	SELECT
-		ar.artista AS `artista`,
-        al.album AS `album`
-	FROM
-		SpotifyClone.Artista AS ar
-			INNER JOIN
-		SpotifyClone.Album AS al ON ar.artista_id = al.artista_id
-    WHERE
-		`artista` = artista_nome;
+SELECT
+	ar.artista AS `artista`,
+	al.album AS `album`
+FROM
+	SpotifyClone.Artista AS ar
+		INNER JOIN
+	SpotifyClone.Album AS al ON ar.artista_id = al.artista_id
+WHERE
+	`artista` = artista_nome;
 END $$
 
 DELIMITER ;
