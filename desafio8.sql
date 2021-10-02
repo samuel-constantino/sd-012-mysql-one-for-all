e004b405a31954fe8c669e35c625308a5db2c4f5
Samuel Constantino <samuelicapuidados@gmail.com>
2021-10-01 22:10:11 -0300

    implementa desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON Usuario
+FOR EACH ROW
+BEGIN
+    DELETE FROM Usuario_Musica AS um
+	WHERE um.usuario_id = OLD.usuario_id;
+    
+    DELETE FROM Usuario_Artista AS ua
+	WHERE ua.usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
